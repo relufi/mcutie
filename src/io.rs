e736f040d5ce19503bff648575b449d702fa5748
relufi <relufi@qq.com>
2025-01-02 15:05:20 +0800

    合并main分支变更

--- src/io.rs
+++ src/io.rs
@@ -1,13 +1,11 @@
 use core::ops::Deref;
 
-use embassy_futures::select::select3;
+use embassy_futures::select::{select, select4};
 use embassy_net::{dns::DnsQueryType, tcp::{TcpReader, TcpSocket, TcpWriter}, IpEndpoint, Stack};
 use embassy_sync::pubsub::WaitResult;
 use embassy_time::{Duration, Timer};
 use embedded_io_async::Write;
-use mqttrs::{
-    decode_slice, Connect, ConnectReturnCode, LastWill, Packet, Pid, Protocol, Publish, QoS, QosPid,
-};
+use mqttrs::{decode_slice_with_len, Connect, ConnectReturnCode, LastWill, Packet, Pid, Protocol, Publish, QoS, QosPid};
 
 use crate::{fmt::Debug2Format, ControlMessage, ControlSubscriber, Error, IpDn, McutieReceiver, McutieSender, MqttMessage, Payload, Publishable, Topic, TopicString, CONFIRMATION_TIMEOUT, DEFAULT_BACKOFF, RESET_BACKOFF};
 
@@ -104,28 +102,6 @@ pub(crate) async fn publish_inner(
     }
 }
 
-fn packet_size(buffer: &[u8]) -> Option<usize> {
-    let mut pos = 1;
-    let mut multiplier = 1;
-    let mut value = 0;
-
-    while pos < buffer.len() {
-        value += (buffer[pos] & 127) as usize * multiplier;
-        multiplier *= 128;
-
-        if (buffer[pos] & 128) == 0 {
-            return Some(value + pos + 1);
-        }
-
-        pos += 1;
-        if pos == 5 {
-            return Some(0);
-        }
-    }
-
-    None
-}
-
 /// The MQTT task that must be run in order for the stack to operate.
 pub struct McutieTask<'t,'a, T, L, const S: usize>
 where
@@ -184,13 +160,12 @@ where
 
             let mut start_pos = 0;
             loop {
-                let packet_length = match packet_size(&buffer[start_pos..cursor]) {
-                    Some(0) => {
-                        error!("Invalid MQTT packet");
-                        return Err(Error::PacketError);
+                let packet = match decode_slice_with_len(&buffer[start_pos..cursor]) {
+                    Ok(Some((packet_length, packet))) => {
+                        start_pos += packet_length;
+                        packet
                     }
-                    Some(len) => len,
-                    None => {
+                    Ok(None) => {
                         // None is returned when there is not yet enough data to decode a packet.
                         if start_pos != 0 {
                             // Adjust the buffer to reclaim any unused data
@@ -199,20 +174,11 @@ where
                         }
                         break;
                     }
-                };
-
-                let packet = match decode_slice(&buffer[start_pos..(start_pos + packet_length)]) {
-                    Ok(Some(p)) => p,
-                    Ok(None) => {
-                        error!("Packet length calculation failed.");
-                        return Err(Error::PacketError);
-                    }
                     Err(_) => {
                         error!("Invalid MQTT packet");
                         return Err(Error::PacketError);
                     }
                 };
-
                 trace!(
                     "Received packet from broker: {:?}",
                     Debug2Format(&packet.get_type())
@@ -297,7 +263,7 @@ where
                     }
                 }
 
-                start_pos += packet_length;
+                // start_pos += packet_length;
                 if start_pos == cursor {
                     cursor = 0;
                     break;
@@ -412,8 +378,17 @@ where
                     let _ = send_packet(self.sender,Packet::Pingreq).await;
                 }
             };
+            let link_down = async {
+                self.network.wait_link_down().await;
+                warn!("Network link lost");
+            };
+            let ip_down = async {
+                self.network.wait_config_down().await;
+                warn!("Network config lost");
+            };
             info!("mq loop start");
-            select3(send_loop, ping_loop, recv_loop).await;
+            // select3(send_loop, ping_loop, recv_loop).await;
+            select4(send_loop, ping_loop, recv_loop, select(link_down, ip_down)).await;
 
             socket.close();
 
